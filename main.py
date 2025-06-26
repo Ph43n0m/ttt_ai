afcb1f44bf6327156a12fbc2bf8d3577b46c01e2
Ph43n0m <bks42@gmx.net>
2025-06-26 20:16:15 +0200

    Refactor Agent and MiniMaxAgent classes, add game statistics tracking, and implement random move selection

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from pathlib import Path
 src_path = Path(__file__).parent / "src"
 sys.path.insert(0, str(src_path))
 
-from ttt_ai.play import main
+from ttt_ai.play_real_game import main
 
 if __name__ == "__main__":
     main()
--- src/ttt_ai/agent.py
+++ src/ttt_ai/agent.py
@@ -10,12 +10,54 @@ LR = 0.001
 
 
 class Agent(ABC):
-    def __init__(self):
+    def __init__(self, field_state_type: FieldState = FieldState.X, randomness: float = 0.2):
+        self.FIELD_STATE_TYPE = field_state_type
+        self.games_won = 0
+        self.games_lost = 0
+        self.games_draw = 0
         self.reward = 0
-        self.n_games = 0
-        self.epsilon = 0  # randomness
+        self.epsilon = randomness  # randomness
         self.gamma = 0.9  # discount rate
         self.memory = deque(maxlen=MAX_MEMORY)  # popleft()
+
+    def get_game_count(self) -> int:
+        """Get the number of games played."""
+        return self.games_won + self.games_lost + self.games_draw
+
+    def update(self, game_won: bool = False, game_lost: bool = False, game_draw: bool = False, ) -> None:
+        if game_won:
+            self.reward += 2
+            self.games_won += 1
+        elif game_lost:
+            self.reward -= 1
+            self.games_lost += 1
+        elif game_draw:
+            self.reward += 1
+            self.games_draw += 1
+
+    def get_wl_ratio(self) -> float:
+        """Calculate the win/loss ration."""
+        if self.games_won < 0 or self.games_lost < 0:
+            print("Invalid game count or games lost. Returning 0.0 for win/loss ratio.")
+            return float(0)
+
+        return (
+            self.games_won / self.games_lost
+            if self.games_lost > 0
+            else float(self.games_won)
+        )
+
+    def get_win_rate(self) -> float:
+        """Calculate the win rate."""
+
+        if self.games_won < 0 or self.get_game_count() < 0:
+            print("Invalid game count or games won. Returning 0.0 for win rate.")
+            return float(0)
+
+        return self.games_won / self.get_game_count() if self.get_game_count() > 0 else float(0)
+
+    def get_best_move(self, board) -> int | None:
+        pass
 
 
 class MiniMaxAgent(Agent):
@@ -23,8 +65,8 @@ class MiniMaxAgent(Agent):
     An agent that uses the minimax algorithm to play Tic Tac Toe.
     """
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, field_state_type: FieldState = FieldState.X, randomness: float = 0.2):
+        super().__init__(field_state_type, randomness)
 
     def get_best_move(self, board) -> int | None:
         """
@@ -33,6 +75,7 @@ class MiniMaxAgent(Agent):
             board: The current state of the Tic Tac Toe board.
         Returns:
             The best move for the current player.
+            :param board:
         """
         # Implement the minimax algorithm to find the best move
 
@@ -43,25 +86,30 @@ class MiniMaxAgent(Agent):
         ):
             return None
 
-        if board.is_empty():
-            return random.choice([0, 2, 6, 8])  # pick a random corner as the first move
+        if random.uniform(0, 1) < self.epsilon:
+            return board.get_flat_index_of_radom_free_field()  # Random move if randomness condition is met
+        else:
+            # If randomness condition is not met, use minimax algorithm
 
-        best_score = float("-inf")
-        best_move = None
-        for row in range(board.BOARD_SIZE):
-            for col in range(board.BOARD_SIZE):
-                if board[row, col].state == FieldState.EMPTY:
-                    # Make the move
-                    board[row, col].state = FieldState.X
-                    score = self._minimax(board, 0, False)
-                    # Undo the move
-                    board[row, col].state = FieldState.EMPTY
+            if board.is_empty():
+                return random.choice([0, 2, 6, 8])  # pick a random corner as the first move
 
-                    if score > best_score:
-                        best_score = score
-                        best_move = board.get_flat_index(row, col)
+            best_score = float("-inf")
+            best_move = None
+            for row in range(board.BOARD_SIZE):
+                for col in range(board.BOARD_SIZE):
+                    if board[row, col].state == FieldState.EMPTY:
+                        # Make the move
+                        board[row, col].state = self.FIELD_STATE_TYPE
+                        score = self._minimax(board, 0, False if self.FIELD_STATE_TYPE == FieldState.X else True)
+                        # Undo the move
+                        board[row, col].state = FieldState.EMPTY
 
-        return best_move
+                        if score > best_score:
+                            best_score = score
+                            best_move = board.get_flat_index(row, col)
+
+            return best_move
 
     def _minimax(self, board, depth, is_maximizing):
         """
@@ -74,11 +122,15 @@ class MiniMaxAgent(Agent):
             The score of the board state.
         """
         # Base case: check for terminal states (win/loss/draw)
-        if board.is_winner(FieldState.X):
+        if board.is_board_full():
+            return 0
+        elif board.is_winner(self.FIELD_STATE_TYPE):
             return float("inf")
-        elif board.is_winner(FieldState.O):
+        elif board.is_winner(FieldState.O if not self.FIELD_STATE_TYPE == FieldState.X else FieldState.X):
             return float("-inf")
-        elif board.is_board_full():
+
+        if depth >= 7:  # Limit the depth to prevent excessive recursion
+            print(f"Critical depth of {depth} reached, returning 0")
             return 0
 
         # Recursive case: explore all possible moves
@@ -87,7 +139,7 @@ class MiniMaxAgent(Agent):
             for row in range(board.BOARD_SIZE):
                 for col in range(board.BOARD_SIZE):
                     if board[row, col].state == FieldState.EMPTY:
-                        board[row, col].state = FieldState.X
+                        board[row, col].state = self.FIELD_STATE_TYPE
                         score = self._minimax(board, depth + 1, False)
                         board[row, col].state = FieldState.EMPTY
                         best_score = max(score, best_score)
@@ -97,7 +149,8 @@ class MiniMaxAgent(Agent):
             for row in range(board.BOARD_SIZE):
                 for col in range(board.BOARD_SIZE):
                     if board[row, col].state == FieldState.EMPTY:
-                        board[row, col].state = FieldState.O
+                        board[
+                            row, col].state = FieldState.O if not self.FIELD_STATE_TYPE == FieldState.X else FieldState.X
                         score = self._minimax(board, depth + 1, True)
                         board[row, col].state = FieldState.EMPTY
                         best_score = min(score, best_score)
